482a2ab5f689c23be4ccf1408dfcc9ace10c8019
Yujin <jini1514@soongsil.ac.kr>
2023-04-11 19:05:46 +0900

    add : HTML 응답, JSP 파일에 java 코드 사용해보기

--- insta/src/main/java/com/example/insta/web/HttpBodyController.java
+++ insta/src/main/java/com/example/insta/web/HttpBodyController.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RestController;
 
 public class HttpBodyController {
 
