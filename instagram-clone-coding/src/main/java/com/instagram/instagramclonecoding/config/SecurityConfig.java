33c7dc6b27ab14902e2d3c05c21587b0531ad589
Yujin <jini1514@soongsil.ac.kr>
2023-05-02 15:56:59 +0900

    feat: SecurityConfig
    solve deprecated

--- instagram-clone-coding/src/main/java/com/instagram/instagramclonecoding/config/SecurityConfig.java
+++ instagram-clone-coding/src/main/java/com/instagram/instagramclonecoding/config/SecurityConfig.java
@@ -1,12 +1,32 @@
 package com.instagram.instagramclonecoding.config;
 
-//import org.springframework.security.WebSecurityConfigurerAdapter
-import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity //해당 파일로 시큐리티를 활성화
-@Configuration //IoC
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+import lombok.RequiredArgsConstructor;
+
+@Configuration // IoC
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**", "/api/**").authenticated() //얘네는 인증 필요
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin") // GET
+                .loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행
+                .defaultSuccessUrl("/");//정상적으로 로그인 되었으면 / 페이지로
+        return http.build();
+    }
 }
